322021d928d21fb907b6c9a0acfe3a1ad17e617f
Ryan Sampson <rvsampson94@gmail.com>
2019-04-08 23:32:36 -0400

    enforce user authentication

---
+++ api/api/middleware.py
@@ -0,0 +1,23 @@
+"""
+Custom middleware classes.
+"""
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+class EnforceAuthMiddleware(object):
+    """
+    Requires the user to be logged in to access the site contents.
+    """
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+        # One-time configuration and initialization.
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        if not request.user.is_authenticated:
+            if (request.path == reverse('login')):
+                return response
+            else:
+                return HttpResponseRedirect(reverse('login'))
+        return response
--- api/api/settings.py
+++ api/api/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'api.middleware.EnforceAuthMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
