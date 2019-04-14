5e2fbe41c3f465ed413f3d36ed6c1685d5d29c8d
Ryan Sampson <rvsampson94@gmail.com>
2019-04-13 22:38:37 -0400

    Fill out fields for models: Customer, Product, Ingredient, Recipe, Subscription, and Transaction

--- api/pawkages/models.py
+++ api/pawkages/models.py
@@ -1,25 +1,48 @@
 from django.db import models
 
+from administration.models import User
+
 
 class Customer(models.Model):
-    pass
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    shipping_address = models.CharField(max_length=125)
+    billing_address = models.CharField(max_length=125)
 
 
 class Product(models.Model):
-    pass
+    name = models.CharField(max_length=25)
+    description = models.CharField(max_length=125)
+    price = models.FloatField()
+    inventory = models.IntegerField()
 
 
 class Ingredient(models.Model):
-    pass
+    name = models.CharField(max_length=25)
+    description = models.CharField(max_length=125)
+    price = models.FloatField(default=0.0)
 
 
 class Recipe(models.Model):
-    pass
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    ingredients = models.ManyToManyField(Ingredient)
+    PET_SIZE_CHOICES = (
+        ('CAT', 'Cat'),
+        ('SM', 'Small'),
+        ('MD', 'Medium'),
+        ('LG', 'Large'),
+    )
+    pet_size = models.CharField(max_length=3, choices=PET_SIZE_CHOICES, default='MD')
 
 
 class Subscription(models.Model):
-    pass
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    recipes = models.ManyToManyField(Recipe)
+    products = models.ManyToManyField(Product)
+    frequency = models.DurationField()
+    active = models.BooleanField(default=True)
 
 
 class Transaction(models.Model):
-    pass
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product)
+    date = models.DateTimeField(auto_now_add=True)
