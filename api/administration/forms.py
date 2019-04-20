0e521be6f6308916c8ec9490ee2cdf0d5c5aad5a
Ryan Sampson <rvsampson94@gmail.com>
2019-04-20 17:40:41 -0400

    apply fixes to signup page

--- api/administration/forms.py
+++ api/administration/forms.py
@@ -6,10 +6,11 @@ class UserCreateForm(UserCreationForm):
     first_name = forms.CharField(max_length=64)
     last_name = forms.CharField(max_length=64)
     email = forms.EmailField(required=True)
+    agreement = forms.BooleanField(required=True)
 
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'email', 'username', 'password1', 'password2')
+        fields = ('first_name', 'last_name', 'email', 'username', 'password1', 'password2', 'agreement')
 
     def save(self, commit=True):
         user = super(UserCreateForm, self).save(commit=False)
