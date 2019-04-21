6b7011337e1dcb16ceb5eb97a06e8abfc075541b
Chris Blackmom <blackmonc21@gmail.com>
2019-04-20 23:50:49 -0400

    Update Contact.vue
    Updated stylings for consistency, added icons to fields and set character limit for contact reason (may need to be validated on backend)

--- frontend/src/components/Contact.vue
+++ frontend/src/components/Contact.vue
@@ -1,42 +1,43 @@
 <template>
-
-  <v-app id="inspire">
-    <v-content>
-      <v-container fluid fill-height>
-        <v-layout align-center justify-center>
-          <v-flex xs12 sm8 md4>
-            <v-card class="elevation-12">
-              <v-toolbar dark color="light-blue">
-                <v-toolbar-title>Contact Us</v-toolbar-title>
-                <v-spacer></v-spacer>
-                <v-tooltip right>
-                </v-tooltip>
-              </v-toolbar>
-              <v-card-text>
-                <v-form>
-                  <v-text-field name="First Name" label="First Name" type="text"></v-text-field>
-                  <v-text-field name="Last Name" label="Last Name" type="text"></v-text-field>
-                  <v-text-field name="Order" label="Order Number (optional)" type="text"></v-text-field>
-                  <v-text-field name="Email" label="Email" type="Email"></v-text-field>
-                  <v-text-field name="Reason for Contact" label="Reason for Contact" type="text"></v-text-field>
-                </v-form>
-              </v-card-text>
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="light-blue">Send</v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-flex>
-        </v-layout>
-      </v-container>
-    </v-content>
-  </v-app>
+  <v-content>
+    <v-container fluid fill-height>
+      <v-layout align-center justify-center>
+        <v-flex xs12 sm8 md4>
+          <v-card class="elevation-12">
+            <v-toolbar dark color="primary">
+              <v-toolbar-title>Contact Us</v-toolbar-title>
+              <v-spacer></v-spacer>
+              <v-tooltip right>
+              </v-tooltip>
+            </v-toolbar>
+            <v-card-text>
+              <v-form>
+                <v-text-field prepend-icon="person" name="First Name" label="First Name" type="text"></v-text-field>
+                <v-text-field prepend-icon="person" name="Last Name" label="Last Name" type="text"></v-text-field>
+                <v-text-field prepend-icon="confirmation_number" name="Order" label="Order Number (optional)" type="text"></v-text-field>
+                <v-text-field prepend-icon="email" name="Email" label="Email" type="Email"></v-text-field>
+                <!--80 character limit for contact reason-->
+                <v-textarea prepend-icon="format_quote" name="Reason for Contact" label="Reason for Contact" type="text" counter="80" :rules="[rules.length(80)]" no-resize></v-textarea>
+              </v-form>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="primary">Send</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-content>
 </template>
 
 <script>
   export default {
     data: () => ({
-      drawer: null
+      drawer: null,
+      rules: {
+        length: len => v => (v || '').length <= len || `Too many characters, please limit your comment to ${len} characters`
+      }
     }),
     props: {
       source: String
