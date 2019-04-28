78dd02eff0bf0b8da33c625456a63c8cf82a0fcd
Ryan Sampson <rvsampson94@gmail.com>
2019-04-28 11:36:24 -0400

    fugly product description page

--- frontend/src/components/ViewProduct.vue
+++ frontend/src/components/ViewProduct.vue
@@ -11,9 +11,41 @@
               <v-tooltip right>
               </v-tooltip>
             </v-toolbar>
-
-
-
+            
+            <v-layout row wrap align-end>
+              <v-flex xs4>
+                <v-card class="product-img">
+                  <v-img :src="product.img_src"/>
+                </v-card>
+              </v-flex>
+              <v-flex xs8>
+                <v-card class="product-info">
+                  <v-card-title>Product Information</v-card-title>
+                  <v-card-text>
+                    <table>
+                      <tr>
+                        <th>{{product.name}}</th>
+                      </tr>
+                      <tr>
+                        <td>${{product.price}}</td>
+                      </tr>
+                      <tr>
+                        <td>{{product.inventory > 0 ? 'Instock: ' + product.inventory + ' left' : 'Out of Stock'}}</td>
+                      </tr>
+                    </table>
+                    <v-btn color="primary">Add to Cart</v-btn>
+                  </v-card-text>
+                </v-card>
+              </v-flex>
+              <v-flex xs12>
+                <v-card class="product-desc">
+                  <v-card-title>Product Description</v-card-title>
+                  <v-card-text>
+                    {{product.description}}
+                  </v-card-text>
+                </v-card>
+              </v-flex>
+            </v-layout>
           </v-card>
         </v-flex>
       </v-layout>
@@ -43,6 +75,15 @@
 
 <style lang="sass">
 
+  .product-img
+    margin: 2em
 
+  .product-info
+    margin-left: 4em
+    margin-right: 2em
+    margin-bottom: 2em
+
+  .product-desc
+    margin: 2em
 
 </style>
