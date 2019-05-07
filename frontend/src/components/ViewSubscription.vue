9148f6b4af04841460f32a22edda2c10667c74b9
blackmonc21 <blackmonc21@gmail.com>
2019-05-07 16:02:37 -0400

    Update ViewSubscription.vue
    Adding functionality to confirm user wants to remove an item
    Item removal implemented

--- frontend/src/components/ViewSubscription.vue
+++ frontend/src/components/ViewSubscription.vue
@@ -32,12 +32,12 @@
             </v-card-text>
 
             <v-card-text><h3>Recipes</h3></v-card-text>
-            <v-data-table :headers="headers" :items="subs.recipes" :show-pagination="false" hide-actions>
+            <v-data-table :headers="headers" :items="subs.recipes" :show-pagination="false" :key="recipeKey" hide-actions>
               <template v-slot:items="props">
                   <td class="text-xs-left">{{ props.item.name }}</td>
                   <td class="text-xs-left">{{ props.item.description }}</td>
                   <td class="text-xs-left">${{ props.item.price }}</td>
-                  <td><v-tooltip bottom><template v-slot:activator="{ on }"><v-icon @click="removeRecipe()" v-on="on">cancel</v-icon></template><span>Remove Item</span></v-tooltip></td>
+                  <td><v-tooltip bottom><template v-slot:activator="{ on }"><v-icon @click="removeRecipe(props.item)" v-on="on">cancel</v-icon></template><span>Remove Item</span></v-tooltip></td>
               </template>
               <template v-slot:no-data>
                   <v-alert :value="true" color="transparent" style="color: rgba(0,0,0,0.54)">
@@ -47,12 +47,14 @@
             </v-data-table>
 
             <v-card-text><h3>Products</h3></v-card-text>
-            <v-data-table :headers="headers" :items="subs.products" :show-pagination="false" hide-actions>
+            <v-data-table :headers="headers" :items="subs.products" :show-pagination="false" :key="productKey" hide-actions>
               <template v-slot:items="props">
                   <td class="text-xs-left">{{ props.item.name }}</td>
                   <td class="text-xs-left">{{ props.item.description }}</td>
                   <td class="text-xs-left">${{ props.item.price }}</td>
-                  <td><v-tooltip bottom><template v-slot:activator="{ on }"><v-icon @click="removeProduct()" v-on="on">cancel</v-icon></template><span>Remove Item</span></v-tooltip></td>
+                  <td><v-tooltip bottom><template v-slot:activator="{ on }"><v-icon @click="removeProduct(props.item)" v-on="on">cancel</v-icon></template><span>Remove Item</span></v-tooltip>
+
+                  </td>
               </template>
               <template v-slot:no-data>
                   <v-alert :value="true" color="transparent" style="color: rgba(0,0,0,0.54)">
@@ -60,6 +62,19 @@
                   </v-alert>
               </template>
             </v-data-table>
+
+            <v-dialog v-model="dialog" max-width="290">
+              <v-card>
+                <v-card-title class="headline">Remove Item?</v-card-title>
+                <v-card-text>By clicking "YES," this item will be removed from your subscription. This action cannot be undone. If you wish
+                to resubscribe to the item, you will have to readd the item to your subscription.</v-card-text>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="green darken-1" flat @click="dialog = false, agreed = false">Disagree</v-btn>
+                  <v-btn color="green darken-1" flat @click="dialog = false, agreed = true">Agree</v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
 
             <v-card-actions>
                 <v-spacer></v-spacer>
@@ -97,6 +112,10 @@ export default {
           value: 'actions'
         }
       ],
+      recipeKey: 0,
+      productKey: 0,
+      dialog: false,
+      agreed: false,
     }),
     methods: {
         getSubs() {
@@ -115,10 +134,8 @@ export default {
 
                         currentRecipe.price = recipeCost.toFixed(2);
                     }
-
-                    console.log(subscriptionData);
-
                     this.subs = subscriptionData;
+
                 }
             )
         },
@@ -129,12 +146,36 @@ export default {
                 }
             );
         },
-        removeRecipe() {
+        removeRecipe(recipe) {
+          var currentRecipe = this.subs.recipes, updatedRecipes = [];
 
+          for (var i = 0; i < currentProducts.length; i++) {
+            if (currentProducts[i].id != product.id) {
+              updatedProducts.push(currentProducts[i]);
+              console.log(currentProducts[i]);
+            }
+          }
+          this.subs.products = updatedProducts;
+          this.recipeKey += 1;
         },
-        removeProduct() {
+        removeProduct(product) {
+          this.dialog = true;
+          console.log(this.agreed);
 
-        }
+          if (this.agreed == true) {
+            console.log("prcoess");
+            var currentProducts = this.subs.products, updatedProducts = [];
+
+            for (var i = 0; i < currentProducts.length; i++) {
+              if (currentProducts[i].id != product.id) {
+                updatedProducts.push(currentProducts[i]);
+                console.log(currentProducts[i]);
+              }
+            }
+            this.subs.products = updatedProducts;
+            this.productKey += 1;
+          }
+        },
     },
     beforeMount() {
         this.getSubs();
