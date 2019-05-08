fe5fc268d55de315d61edaf13c75e297fc37ea23
Ryan Sampson <rvsampson94@gmail.com>
2019-05-08 00:24:31 -0400

    Require verification when user changes email

--- api/api/settings.py
+++ api/api/settings.py
@@ -154,3 +154,4 @@ LOGIN_REDIRECT_URL = '/'
 
 EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
+EMAIL_HOST_USER = 'noreply@pawkages.com'
