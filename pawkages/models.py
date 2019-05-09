decc0b72cf0c2da09506ccc2bc9d5d4e1c6cefdf
Ryan Sampson <rvsampson94@gmail.com>
2019-05-09 00:41:31 -0400

    increase field lengths on CharFields

--- pawkages/models.py
+++ pawkages/models.py
@@ -5,30 +5,30 @@ from administration.models import User
 
 class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-    shipping_address = models.CharField(max_length=125)
-    billing_address = models.CharField(max_length=125)
+    shipping_address = models.CharField(max_length=256)
+    billing_address = models.CharField(max_length=256)
 
 
 class Product(models.Model):
     img_src = models.URLField()
-    name = models.CharField(max_length=25)
-    description = models.CharField(max_length=250)
+    name = models.CharField(max_length=256)
+    description = models.CharField(max_length=512)
     price = models.FloatField()
     inventory = models.IntegerField()
 
 
 class Ingredient(models.Model):
     img_src = models.URLField(default='')
-    name = models.CharField(max_length=25)
-    description = models.CharField(max_length=250)
+    name = models.CharField(max_length=256)
+    description = models.CharField(max_length=512)
     price = models.FloatField(default=0.0)
 
 
 
 class Recipe(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, default = 1)
-    name = models.CharField(max_length=250, default = 'New Recipe')
-    description = models.CharField(max_length=500, default = '')
+    name = models.CharField(max_length=256, default = 'New Recipe')
+    description = models.CharField(max_length=512, default = '')
     ingredients = models.ManyToManyField(Ingredient)
     PET_SIZE_CHOICES = (
         ('CAT', 'Cat'),
@@ -54,10 +54,10 @@ class Transaction(models.Model):
 
 
 class Feedback(models.Model):
-    firstname = models.CharField(max_length=25)
-    lastname = models.CharField(max_length=25)
+    firstname = models.CharField(max_length=64)
+    lastname = models.CharField(max_length=64)
     order_number = models.IntegerField()
-    email = models.CharField(max_length=50)
-    reason = models.CharField(max_length=250)
+    email = models.CharField(max_length=64)
+    reason = models.CharField(max_length=256)
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     date = models.DateTimeField(auto_now_add=True)
