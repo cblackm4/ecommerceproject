fed80f5704a98d26e78ee705366b43668ef8e7c4
Ryan Sampson <rvsampson94@gmail.com>
2019-05-09 20:29:09 -0400

    I was wrong

--- scripts/resetdb.sh
+++ scripts/resetdb.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-cd ./api
-
 #remove migrations files and db
 rm -rf ./administration/migrations ./pawkages/migrations
 rm -f db.sqlite3
@@ -12,5 +10,3 @@ python manage.py migrate
 
 # create super user account
 echo "from administration.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'password')" | python manage.py shell
-
-cd ..
